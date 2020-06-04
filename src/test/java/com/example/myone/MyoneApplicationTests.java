2c16986ae97a38a70800b123792948089b92b62a
gyx19991222 <1906022969@qq.com>
2020-06-04 08:44:10 +0800

    Initial commit

---
+++ src/test/java/com/example/myone/MyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
